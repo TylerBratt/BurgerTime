5a586a1eee35f13ca2a737f4ec574763e7df20e4
Austin <sicardaustin97@gmail.com>
2021-06-04 10:47:13 -0400

    Working Backend and React

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import useApplicationData from './hooks/useApplicationData'
 const App = () => {
   const {
     state,
