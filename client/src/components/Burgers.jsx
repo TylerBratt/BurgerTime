0224d6baf191bcf0c07faa3cb23ae33d659edacd
Tyler Bratt <tyler~bratt@hotmail.com>
2021-06-04 12:24:28 -0400

    add component templates

---
+++ client/src/components/Burgers.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Burgers(props) {
+  return <h1>Burgers Page</h1>
+};
---
+++ client/src/components/Favourites.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Favourites(props) {
+  return <h1> Favourites Page</h1>
+};
